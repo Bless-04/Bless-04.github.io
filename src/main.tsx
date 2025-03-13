d90412257f6c8ca912d5ccc87526e5c58f108a0b
Blessing <96212794+Bless-04@users.noreply.github.com>
2025-03-13 10:33:48 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 const element: string = "root"; //the html element that makes website work
 const rootElement: HTMLElement | null = document.getElementById(element);
 
-if (rootElement == null) console.error(`{${element}} not found`);
+if (rootElement == null) console.error(`There is no {${element}} element in the html document`);
 
 ReactDOM.createRoot(rootElement ?? document.createElement(element)).render(
 	<React.StrictMode>
