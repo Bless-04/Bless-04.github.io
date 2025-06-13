6c82ffaa2bb104df687ca181bdbe58fd958e7167
Blessing <babumere04@gmail.com>
2025-06-12 23:14:24 -0400

    useless images deleted

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,9 +4,9 @@ export { default as gmail } from './gmail.svg';
 export { default as favicon } from './favicon.jpeg';
 export { default as github } from './github.png';
 export { default as menu } from './menu.svg';
-export { default as close } from './close.svg';
 export { default as linkedin } from './linkedin.svg';
 export { default as instagram } from './instagram.svg'
+export { default as roblox_sharp } from './roblox_sharp.png'
 
 ///#region Tech
 export { default as cpp } from './tech/cpp.png';
