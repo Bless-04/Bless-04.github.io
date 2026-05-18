29d803abfd88313234fd852e4187860e51f894b6
Blessing <babumere04@gmail.com>
2026-05-18 15:27:32 -0400

    readd

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+// General?
+export { default as resume } from './resume.pdf';
+export { default as gmail } from './gmail.svg';
+export { default as favicon } from './favicon.jpeg';
+export { default as github } from './github.png';
+export { default as menu } from './menu.svg';
+export { default as linkedin } from './linkedin.svg';
+export { default as instagram } from './instagram.svg'
+export { default as roblox_sharp } from './roblox_sharp.png'
+export { default as cve_scout } from './cve_scout.png'
+
+///#region Tech
+
+//#endregion
+
+//#region Company
+
+//#endregion
+// Projects
---
+++ src/assets/tech/index.js
@@ -0,0 +1,88 @@
+import cpp from './cpp.png';
+import c from './c.png';
+import csharp from './csharp.png';
+import css from './css.png';
+import compose from "./compose.png";
+import retrofit from './retrofit.png';
+import hilt from './google-dagger.png';
+import dotnet from './dotnet.svg';
+import docker from './docker.png';
+import git from './git.png';
+import html from './html.png';
+import java from './java.jpg';
+import kotlin from './kotlin.png';
+import rust from './rust.png';
+import javascript from './javascript.png';
+import mongodb from './mongodb.png';
+import nodejs from './nodejs.png';
+import reactjs from './reactjs.png';
+import tailwind from './tailwind.png';
+import typescript from './typescript.png';
+import threejs from './threejs.png';
+import winforms from './winforms.png'
+import avalonia from './avalonia.png'
+import xunit from './xunit.png'
+import nunit from './nunit.svg'
+import junit from './junit.png'
+import dotnetCORE from './dotnetcore.png'
+import ASPdotnet from './asp.net.png'
+import expressjs from './express.js.png'
+import mstest from './mstest.png'
+import framermotion from './framer-motion.svg'
+import postgreSQL from './postgresql.png'
+import sqlite from './sqlite.svg'
+import MySQL from './mysql.png'
+import EFCore from './ef-core.png'
+import mongoose from './mongoose.png'
+import xaml from './xaml.png'
+import xaml2 from './xaml2.png'
+import subversion from './subversion.webp'
+import postman from './postman.png'
+import lepoco_wpfui from './lepoco.wpfui.png'
+import vmware from './vmware.png'
+import spring from './spring.png'
+export const tech = {
+    cpp,
+    c,
+    compose,
+    csharp,
+    css,
+    dotnet,
+    docker,
+    kotlin,
+    hilt,
+    git,
+    html,
+    sqlite,
+    MySQL,
+    java,
+    javascript,
+    mongodb,
+    nodejs,
+    retrofit,
+    rust,
+    reactjs,
+    tailwind,
+    typescript,
+    threejs,
+    winforms,
+    avalonia,
+    xunit,
+    nunit,
+    junit,
+    dotnetCORE,
+    ASPdotnet,
+    expressjs,
+    mstest,
+    framermotion,
+    postgreSQL,
+    EFCore,
+    mongoose,
+    xaml,
+    xaml2,
+    subversion,
+    postman,
+    lepoco_wpfui,
+    vmware,
+    spring
+}
